ee530a214b1aa1dcace2b7fc685b9c1a0357d5c9
andresgamboaa <andres24k@gmail.com>
2022-10-19 19:58:07 -0600

    Add default component to rootComponent

--- addons/goduz/classes/root_component.gd
+++ addons/goduz/classes/root_component.gd
@@ -8,14 +8,5 @@ func _init():
 func view():
 	return\
 	control({preset="full"}, [
-		box({
-			preset="center", 
-			custom_minimum_size=Vector2(100,100),
-			background_color=Color.LIGHT_CORAL,
-			border_width=6,
-			border_color=Color.BLACK,
-			border_radius=60
-		}, [
-			label({preset="text-align-center-h", text="box"})
-		])
+		GoduzCounter.new()
 	])
